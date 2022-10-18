6c48317473186b5db27eedf8a4adf95904b309e2
keldenl <keldenlin@gmail.com>
2022-10-17 20:28:05 -0700

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { DateTime } from 'luxon';
 
-import { Link } from '@mui/material';
+import { Link, Typography } from '@mui/material';
 import TwitterIcon from '@mui/icons-material/Twitter';
 
 import './Footer.css'
@@ -10,8 +10,12 @@ import './Footer.css'
 export function Footer({ }) {
     return (
         <div className='footer'>
-            <p>All data sourced from Riot's Official Wild Rift <Link target='__blank' href='https://lolm.qq.com/act/a20220818raider/index.html'>CN Dia+ Statistics</Link></p>
-            <p>Built by <Link target='__blank' href='https://twitter.com/RepotedWR'><TwitterIcon fontSize={'10px'} />RepotedWR</Link> © {DateTime.now().year}</p>
+            <Typography variant='caption'>
+                All data sourced from Riot's Official Wild Rift <Link target='__blank' href='https://lolm.qq.com/act/a20220818raider/index.html'>CN Dia+ Statistics</Link> and <Link target='__blank' href='https://wildrift.leagueoflegends.com/en-us/champions/'>Champion List</Link>.
+            </Typography>
+            <Typography variant='caption'>
+                Built by <Link target='__blank' href='https://twitter.com/RepotedWR'><TwitterIcon fontSize={'10px'} />RepotedWR</Link> © {DateTime.now().year}
+            </Typography>
         </div>
     )
 }
